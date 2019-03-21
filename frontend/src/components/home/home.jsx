bd840c9a4793486eb7e6f6c3957e011fa5665a3c
achen1738 <ac99@rice.edu>
2019-03-21 00:31:57 -0500

    Finished left side of content page

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -6,22 +6,31 @@ class Home extends Component {
     super(props);
 
     this.state = {
-      pledges: [],
+      pledges: [
+        "Eat more plant-based meals",
+        "Use more alternative transportation",
+        "Replace disposable items with reusables"
+      ],
       userInfo: {},
-      userPledge: {}
+      userPledge: {},
+      percentile: 0
     };
   }
 
   componentDidMount = async () => {
     var respPledges = await fetch("http://localhost:5000/pledges");
     var respUserInfo = await fetch("http://localhost:5000/user/arthur01");
+    var respPercentile = await fetch(
+      "http://localhost:5000/percentile/arthur01"
+    );
     const pledges = await respPledges.json();
     const userInfo = await respUserInfo.json();
+    const percentile = await respPercentile.json();
     console.log(pledges, userInfo);
     this.setState({
-      pledges: pledges,
       userInfo: userInfo[0],
-      userPledge: userInfo[1]
+      userPledge: userInfo[1],
+      percentile: percentile
     });
   };
 
@@ -34,8 +43,14 @@ class Home extends Component {
     // console.log(resp);
     var info = this.state.userInfo;
     info.score = resp;
+    var pledge = this.state.userPledge;
+    pledge.pledgeScore = isIncrement
+      ? pledge.pledgeScore + 1
+      : pledge.pledgeScore === 0
+      ? pledge.pledgeScore
+      : pledge.pledgeScore - 1;
     // console.log(info);
-    this.setState({ userInfo: info });
+    this.setState({ userInfo: info, userPledge: pledge });
   };
 
   render() {
@@ -43,36 +58,71 @@ class Home extends Component {
       <div className={styles.homeWrapper}>
         <div className={styles.left}>
           <div className={styles.mainScore}>
-            <div className={styles.personalScore}>
-              <span>{this.state.userInfo.score}</span>
-              <div className={styles.personalShadow} />
+            <div className={styles.pledgeName}>
+              {this.state.pledges[this.state.userInfo.pledgeNumber]}
             </div>
+            <div className={styles.scoreContainer}>
+              <div
+                onClick={e => this.updateScore(false)}
+                className={styles.leftCircle}
+              />
+              <div
+                onClick={e => this.updateScore(true)}
+                className={styles.rightCircle}
+              />
+              <span
+                onClick={e => this.updateScore(false)}
+                style={{ marginRight: "0.5vw", left: "0.5vw" }}
+              >
+                -
+              </span>
+              <div className={styles.personalScore}>
+                <span>{this.state.userInfo.score}</span>
+              </div>
+              <span
+                onClick={e => this.updateScore(true)}
+                style={{ marginLeft: "0.5vw", right: "0.5vw" }}
+              >
+                +
+              </span>
+            </div>
+
             <div
-              className={[styles.smallerScoreBorder, styles.leftScore].join(
+              className={[styles.smallerScoreContainer, styles.leftScore].join(
                 " "
               )}
             >
-              <div className={styles.smallerScore}>
-                <span>{this.state.userPledge.pledgeScore}</span>
+              <div className={[styles.smallerScoreBorder].join(" ")}>
+                <div className={styles.smallerScore}>
+                  <span>{this.state.userPledge.pledgeScore}</span>
+                </div>
               </div>
+              <div className={styles.smallerScoreName}>Group Pledge Score</div>
             </div>
             <div
-              className={[styles.smallerScoreBorder, styles.middleScore].join(
+              className={[
+                styles.smallerScoreContainer,
+                styles.middleScore
+              ].join(" ")}
+            >
+              <div className={[styles.smallerScoreBorder].join(" ")}>
+                <div className={styles.smallerScore}>
+                  <span>{this.state.percentile}</span>
+                </div>
+              </div>
+              <div className={styles.smallerScoreName}>Percentile</div>
+            </div>
+            <div
+              className={[styles.smallerScoreContainer, styles.rightScore].join(
                 " "
               )}
             >
-              <div className={styles.smallerScore}>
-                <span>asdf</span>
+              <div className={[styles.smallerScoreBorder].join(" ")}>
+                <div className={styles.smallerScore}>
+                  <span>{this.state.userPledge.members}</span>
+                </div>
               </div>
-            </div>
-            <div
-              className={[styles.smallerScoreBorder, styles.rightScore].join(
-                " "
-              )}
-            >
-              <div className={styles.smallerScore}>
-                <span>{this.state.userPledge.members}</span>
-              </div>
+              <div className={styles.smallerScoreName}>Number of Members</div>
             </div>
             {/* <Button onClick={e => this.updateScore(true)}>Increment!</Button>
             <Button onClick={e => this.updateScore(false)}>Decrement!</Button> */}
