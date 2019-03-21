be42b7021812a758089eea9ba2e879380db8d380
Peter Dulworth <pdulworth97@gmail.com>
2019-03-20 21:23:21 -0500

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from "react";
 import "./App.css";
 import Login from "./components/login/login";
 import Home from "./components/home/home";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Switch>
+          <Route exact path="/" component={loginPage} />
+          <Route exact path="/dashboard" component={dashboardPage}/>
+          {/* <Route component={errorPage}/> */}
+        </Switch>
       </div>
     );
   }
 }
+
+const loginPage = (match) => (
+  <Login />
+);
+
+const dashboardPage = (match) => (
+  <Home />
+);
+
+{/* <Login symbol={match.location.pathname}/> */}
 
 export default App;
