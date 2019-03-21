23fb877d7b7978e9e678f14df2541e6ac0af0bfa
achen1738 <ac99@rice.edu>
2019-03-20 21:24:11 -0500

    Set up the states in login

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,57 +1,170 @@
 import React, { Component } from "react";
 // import Button from "react-bootstrap/Button";
 // import FormControl from "react-bootstrap/FormControl";
-import { FormControl, Button } from "react-bootstrap";
 import styles from "./login.module.css";
-import Avacado from "./avacado.jpg";
 import Avacado2 from "./YUM2.png";
 
 class Login extends Component {
   constructor(props) {
     super(props);
-    
-    this.state = { 
+
+    this.state = {
       formVisible: false,
+      pledge: -1,
+      loginUsername: "",
+      loginPassword: "",
+      signUpUsername: "",
+      signUpPassword: "",
+      error: false
     };
   }
 
-  handleSignup = () => {
-    this.setState({ formVisible: true });
+  handleSignup = async () => {
+    const signUpUsername = this.state.signUpUsername;
+    const signUpPassword = this.state.signUpPassword;
+    const pledge = this.state.pledge;
+    if (!this.state.formVisible) {
+      this.setState({ formVisible: true });
+    } else {
+      if (pledge != -1 && signUpPassword !== "" && signUpUsername !== "") {
+        const res = await fetch(
+          `http://localhost:5000/user/${
+            this.state.signUpUsername
+          }/${signUpPassword}/${pledge}`,
+          {
+            method: "POST"
+          }
+        );
+        const accountMade = await res.json();
+        console.log(accountMade);
+        if (accountMade) {
+          this.props.history.replace("/dashboard");
+        } else {
+          this.setState({ error: true });
+        }
+      }
+    }
   };
 
   handleLogin = () => {};
 
-  render() {
+  updateSignUpUsername = evt => {
+    // console.log(evt.target.value);
+    this.setState({
+      signUpUsername: evt.target.value
+    });
+  };
 
+  updateSignUpPassword = evt => {
+    // console.log(evt.target.value);
+    this.setState({
+      signUpPassword: evt.target.value
+    });
+  };
+
+  updateLoginUsername = evt => {
+    // console.log(evt.target.value);
+    this.setState({
+      loginUsername: evt.target.value
+    });
+  };
+
+  updateLoginPassword = evt => {
+    // console.log(evt.target.value);
+    this.setState({
+      loginPassword: evt.target.value
+    });
+  };
+
+  setPledge = e => {
+    const index = parseInt(e.currentTarget.getAttribute("data-index"));
+    console.log(index);
+    this.setState({ pledge: index });
+  };
+
+  render() {
     let signupForm = null;
-    
+
+    let errorMessage = (
+      <div className={styles.errorMessage}>
+        <span>Sorry username exists already</span>
+      </div>
+    );
+
     if (this.state.formVisible) {
-      signupForm = (<div className={styles.signupForm}>
-        <input type="text" id="uname" name="username" placeholder="username..." /> 
-        <input type="text" id="uname" name="username" placeholder="password..." /> 
-        <div className={[styles.pledgeCheck, styles.c1].join(' ')}>Eat more plant-based meals.</div>
-        <div className={[styles.pledgeCheck, styles.c2].join(' ')}>Use more alternative transportation.</div>
-        <div className={[styles.pledgeCheck, styles.c3].join(' ')}>Replace disposable items with reusables.</div>
-      </div>);
+      signupForm = (
+        <div className={styles.signupForm}>
+          {this.state.error ? errorMessage : null}
+          <input
+            type="text"
+            id="uname"
+            name="username"
+            placeholder="username..."
+            onChange={e => this.updateSignUpUsername(e)}
+          />
+          <input
+            type="text"
+            id="uname"
+            name="username"
+            placeholder="password..."
+            onChange={e => this.updateSignUpPassword(e)}
+          />
+          <div
+            data-index="0"
+            onClick={e => this.setPledge(e)}
+            className={[styles.pledgeCheck, styles.c1].join(" ")}
+          >
+            Eat more plant-based meals.
+          </div>
+          <div
+            data-index="1"
+            onClick={e => this.setPledge(e)}
+            className={[styles.pledgeCheck, styles.c2].join(" ")}
+          >
+            Use more alternative transportation.
+          </div>
+          <div
+            data-index="2"
+            onClick={e => this.setPledge(e)}
+            className={[styles.pledgeCheck, styles.c3].join(" ")}
+          >
+            Replace disposable items with reusables.
+          </div>
+        </div>
+      );
     }
-    
+
     return (
       <div className={styles.loginWrapper}>
         <div className={styles.left}>
-          <img src={Avacado2} alt="asdfasdf"/>
+          <img src={Avacado2} alt="asdfasdf" />
         </div>
 
         <div className={styles.right}>
           <div className={styles.loginForm}>
-            <input type="text" id="uname" name="username" placeholder="username..." /> 
-            <input type="text" id="pass" name="password" placeholder="password..." />  
-            <div className={styles.loginBtn} >Log in</div>
+            <input
+              type="text"
+              id="uname"
+              name="username"
+              placeholder="username..."
+              onChange={e => this.updateLoginUsername(e)}
+            />
+            <input
+              type="text"
+              id="pass"
+              name="password"
+              placeholder="password..."
+              onChange={e => this.updateLoginPassword(e)}
+            />
+            <div className={styles.loginBtn}>Log in</div>
           </div>
 
           <h2>s u s t a i n a b i l i t y</h2>
-          <div className={(this.state.formVisible ? styles.signupWrapper : "")}>
+          <div className={this.state.formVisible ? styles.signupWrapper : ""}>
             {signupForm}
-            <div className={styles.signupBtn} onClick={this.handleSignup}>Sign Up</div>
+            <div className={styles.signupBtn} onClick={this.handleSignup}>
+              Sign Up
+            </div>
           </div>
         </div>
       </div>
@@ -59,7 +172,8 @@ class Login extends Component {
   }
 }
 
-{/* <div className={styles.loginWrapper}>
+{
+  /* <div className={styles.loginWrapper}>
         <form onSubmit={this.handleSubmit} className={styles.login}>
           <h1>Sign Up!</h1>
           <FormControl
@@ -78,6 +192,7 @@ class Login extends Component {
             Submit
           </Button>
         </form>
-      </div> */}
+      </div> */
+}
 
 export default Login;
