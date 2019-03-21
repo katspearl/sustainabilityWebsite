2bba8204b75f7ad3b525767c87e4fce98be0b1e1
Peter Dulworth <pdulworth97@gmail.com>
2019-03-21 00:34:38 -0500

    shit

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styles from "./home.module.css";
+import stylesR from "./homeRight.module.css";
 
 class Home extends Component {
   constructor(props) {
@@ -79,53 +80,89 @@ class Home extends Component {
           </div>
           <div className={styles.secondaryScores} />
         </div>
-        <div className={styles.right}>
-          <div>
-            <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Take
-            your number 6 plastic to the station during Willy Week
+        <div className={stylesR.right}>
+          <Title title={"Pledge Totals"} />
+          <div className={stylesR.pledgeWrapper}>
+            <Pledge val={11}> alternative modes of transportation used</Pledge>
+            <Pledge val={23}> plant based meals eaten</Pledge>
+            <Pledge val={333}> disposable items replaced with reusable items</Pledge>
           </div>
-          <div>
-            <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Buy
-            one Fair Trade certified product (https://pin.it/gjtwq5tmyjfiis)
+          <Title title={"Sustainability Checklist"} />
+          <div className={stylesR.checklistWrapper}>
+            <div>
+              <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Take
+              your number 6 plastic to the station during Willy Week
+            </div>
+            <div>
+              <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Buy
+              one Fair Trade certified product (<a href="https://pin.it/gjtwq5tmyjfiis">link</a>).
+            </div>
+            <div>
+              <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Take
+              your e-waste to a Rice e-waste station (<a href="https://facilities.rice.edu/recycling/special">link</a>).
+            </div>
+            <div>
+              <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Visit
+              the Rice Farmers Market (<a href="https://farmersmarket.rice.edu/">link</a>).
+            </div>
+            <div>
+              <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Bring
+              your own cup to a party
+            </div>
+            <div>
+              <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Bring
+              your own mug to Coffeehouse or East West
+            </div>
+            <div>
+              <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Sign
+              up for a weekly fact with RISE Today
+            </div>
+            <div>
+              <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Wash
+              your clothes with cold water
+            </div>
+            <div>
+              <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Cook
+              one meal for yourself (<a href="https://www.favfamilyrecipes.com/easy-college-recipes-for-college-students/">link</a>).
+            </div>
+            <div>
+              <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Admire
+              our newly made recycling bin signs!
+            </div>
           </div>
+          <Title title={"Leaderboard"} />
           <div>
-            <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Take
-            your e-waste to a Rice e-waste station
-            (https://facilities.rice.edu/recycling/special)
-          </div>
-          <div>
-            <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Visit
-            the Rice Farmers Market (https://farmersmarket.rice.edu/)
-          </div>
-          <div>
-            <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Bring
-            your own cup to a party
-          </div>
-          <div>
-            <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Bring
-            your own mug to Coffeehouse or East West
-          </div>
-          <div>
-            <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Sign
-            up for a weekly fact with RISE Today
-          </div>
-          <div>
-            <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Wash
-            your clothes with cold water
-          </div>
-          <div>
-            <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Cook
-            one meal for yourself
-            (https://www.favfamilyrecipes.com/easy-college-recipes-for-college-students/)
-          </div>
-          <div>
-            <input type="checkbox" name="vehicle1" value="Bike" /> &nbsp; Admire
-            our newly made recycling bin signs!
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf laasasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf laasasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf laasasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+            asasdfjasdj asdjfla sjdflkaj dlfjalkd fjalskjf lasdjf lasdjf lkasjdf lasjdlf ajsdlfk jasdlfj alskdjf laksdjf la
+
+
+
           </div>
         </div>
       </div>
     );
   }
+}
+
+function Title(props) {
+  return (<div className={stylesR.title} >{props.title}</div>);
+}
+
+function Pledge(props) {
+  return (<div className={stylesR.pledge}><span className={stylesR.pledgeVal}>{props.val}</span>{props.children}</div>);
 }
 
 export default Home;
